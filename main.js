80955e7aeab0029a284868a22c6fc8dd213c87ca
Aditya Rao <adicoolrao@gmail.com>
2015-06-29 19:11:22 -0400

    Updating main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+var app = require('./lib/app'),
+    MongoDBHelper = require('./lib/mongodbhelper'),
+    filtergen = require('./lib/filter_gen'),
+    filter_diff = require('./lib/filter_diff');
+
+
+/*var datastore = new MongoDBHelper(undefined,function(err) {
+    if (err) {
+        console.err("Cannot connect to mongodb, error:" + err);
+        process.exit(1);
+    }
+    //exec_server();
+});*/
+
+
+var server = new app();
+
+server.start(undefined,3130);
+
+
+function exec_server() {
+    var date = new Date();
+    date = date.getUTCDate() + ' ' + date.getUTCMonth() + 
+            ' ' + date.getUTCFullYear();
+    var testfun = function(err,data) {
+        console.log('Entering test function');
+        if (err) return console.log(err);
+        console.log('Checking if filters were added for date:' + date);
+        datastore.getFilter(date,'1',function(err,result) {
+            if (err) return console.log(err);
+            console.log(result);
+        });
+    };
+
+    filtergen.generateFilterDummy(datastore,date,'./test/serials_1',testfun);
+    testfun();
+}
