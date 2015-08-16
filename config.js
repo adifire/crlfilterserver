601fc4990de0c76a90c0dba4b594877e64441864
Aditya Rao <adicoolrao@gmail.com>
2015-08-16 13:43:11 -0400

    Updating to real filters

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ config.mongodb.filter_collection = 'filter';
 config.web.port = process.env.WEB_PORT || 3130;
 config.web.host = '127.0.0.1';
 config.cronjob.cronTime = '00 */1 * * * *';
-config.preload.serial_files = ['./test_serials/serials_1','./test_serials/serials_2','./test_serials/serials_3','./test_serials/serials_4'];
+config.preload.serial_files = [
+'./test_serials/filters_type0','./test_serials/filters_type1','./test_serials/filters_type2','./test_serials/filters_type3','./test_serials/filters_type4'];
 
 
 module.exports = config;
