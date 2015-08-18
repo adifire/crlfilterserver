7197552e4faa2953d946dee5d0e035913381ccce
Aditya Rao <adicoolrao@gmail.com>
2015-08-18 19:31:33 -0400

    Fixing redis and adding more caching, small fix to filter_diff and mongodbhelper
    Changed filter_diff to require only two filters as input, no passing of
    datastore access
    Added filter caching in redis
    Fixed caching in filter api handling
    Added less than or equal to check for filters in mongoldb

--- routes/index.js
+++ routes/index.js
@@ -11,40 +11,26 @@ router.get('/',function(req,res){
     res.end('Welcome to CRLFilter app!');
 });
 
-router.get('/filter',function(req,res){
+router.get('/filter',function(req,res) {
     var q = req.query;
     console.log(q);
-    var date = (/^\d{8}$/.test(date)) ? date : 'current',
-        type = q.type || '1',
-        curdate = utils.getFormattedDate();
+    var date = testDate(q.date) ? parseInt(q.date) : curDate(),
+        type = testType(q.type) ? '1' : q.type,
+        curdate = curDate();
 
-    console.log(date);
+    console.log('Date to check: ' + date);
     /*
      * If old date, check if diff exists. If not generate 
      * and store diff also
      */
-    if (date === curdate || date === 'current') {
+    if (date >= curdate) {
+        console.log('Return latest filter');
         // Return current filter
-        date = 'current';
-        return dbconnector(function(err,store) {
-            if (err) {
-                throw err;
-            }
-
-            store.getFilter(date,type,function(err,result) {
-                if (err) {
-                    return res.end('Error');
-                }
-
-                return res.json({
-                    'date': curdate,
-                    'type': type,
-                    'filter': result
-                });
-            });
-        });
+        date = curdate;
+        return getLatestFilter(res,date,type);
     }
 
+    console.log('Fetch the diff if exists');
     /*
      * Try to fetch the diff, if not present in redis store 
      * then generate, store and send diff
@@ -53,7 +39,6 @@ router.get('/filter',function(req,res){
         if (err || !result || result === 0) {
             return genDiff(curdate,date,type,res);
         }
-        console.log(result);
         res.json({'date': curdate,'type': type,'diff': result});
     });
     
@@ -70,7 +55,7 @@ router.get('/config',function(req,res) {
     res.end('Not implemented yet');    
 });
 
-var dbconnector = function(callback) {
+function dbconnector(callback) {
     var store = new mongodb(datastoreurl,function(err) {
         if (err) {
             return callback(err);
@@ -78,36 +63,81 @@ var dbconnector = function(callback) {
 
         return callback(undefined,store);
     });
-};
+}
 
-var genDiff = function(curdate,olddate,type,response) {
+function genDiff(curdate,olddate,type,response) {
     dbconnector(function(err,mongodb) {
         if (err) {
             throw err;
         }
 
-        filterdiff.generateDiff(mongodb,redis,olddate,curdate,type,
-        function(err,res) {
-            if (err) {
-                return mongodb.getFilter('current',type,function(err,res) {
-                    if (err) {
-                        throw err;
+        mongodb.getFilter(olddate,type,function(err1,oldfilter) {
+            mongodb.getFilter(curdate,type,function(err2,newfilter) {
+                if (err1 || !oldfilter || oldfilter.length === 0) {
+                    if (err2) {
+                        throw err2;
                     }
-
-                    return response.json({
-                        'date': curdate,
-                        'type': type,
-                        'filter': res
-                    });
+                    return getLatestFilter(response,curdate,type);
+                }
+                if (err2) {
+                    throw err2;
+                }
+                var diff = filterdiff.generateDiff(oldfilter,newfilter);
+                redis.storeDiff(curdate,olddate,type,diff);
+                response.json({
+                    'date': curdate,
+                    'type': type,
+                    'diff': diff
                 });
-            }
-            response.json({
-                'date': curdate,
-                'type': type,
-                'diff': res
             });
         });
     });
-};
+}
+
+function getLatestFilter(response,date,type) {
+    redis.getFilter(date,type,function(err,result) {
+        if (err || !result || result === 0) {
+            return dbconnector(function(err,store) {
+                if (err) {
+                    throw err;
+                }
+
+                store.getFilter(date,type,function(err,result) {
+                    if (err) {
+                        return response.end('Error');
+                    }
+
+                    redis.storeFilter(date,type,result);
+                    returnFilter(response,date,type,result);
+                });
+            });
+        }
+        console.log('Fetching from redis');
+        return returnFilter(response,date,type,result);
+    });
+}
+
+function returnFilter(res,date,type,filter) {
+    return res.json({
+        'date': date,
+        'type': type,
+        'filter': filter
+    });
+}
+
+function curDate() {
+    return utils.getFormattedDate();
+}
+
+// Returns true if date matches requirements
+function testDate(date) {
+    return (/^\d{8}$/.test(date));
+}
+
+// Returns true if type does not match requirements
+function testType(type) {
+    return !type || isNaN(parseInt(type)) || 
+           parseInt(type) < 0 || parseInt(type) > 4;
+}
 
 module.exports = router;
